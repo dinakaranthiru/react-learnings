ff7e7ebb628f2584e03489fd1a33a7b1584875e4
Dina <dinakarant09@gmail.com>
2026-09-07 12:24:07 +0530

    Add Props component to display name and age

---
+++ API/api-fetching/src/Hooks/Props.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Props = (props) => {
+  return (
+    <div>
+        <h2>Name:{props.name}</h2>
+        <h2>Age:{props.age}</h2>
+    </div>
+  )
+}
+
+export default Props
